0ec048673a30cf262a55518e4d8473b3a98efe3e
FrancisRalph <francis341593@gmail.com>
2021-12-26 20:38:28 +0800

    add custom User class - has necessary properties for assignment - changed key type from string to Guid

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,10 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 
 namespace ApplicationSecurity.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace ApplicationSecurity.Data
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string CreditCardNumber { get; set; }
+        
+        public string CreditCardSecurityCode { get; set; } // CVV or CVC
+        
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+        
+        public string PhotoPath { get; set; }
+        
+    }
+}
