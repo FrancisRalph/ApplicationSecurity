d5e415c7ada0cb045807702faa02c3fb701c392d
FrancisRalph <francis341593@gmail.com>
2022-02-06 05:30:04 +0800

    add RemoveUnnecessary ef migration

---
+++ Data/Migrations/20220205212935_RemoveUnnecessary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ApplicationSecurity.Migrations
+{
+    public partial class RemoveUnnecessary : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreditCardSecurityCode",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreditCardSecurityCode",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
