2a0a52f79f61a0096354362726d5a6fc92aa5984
FrancisRalph <francis341593@gmail.com>
2022-02-08 01:25:01 +0800

    allow admin page only for specific user

--- Pages/Admin.cshtml.cs
+++ Pages/Admin.cshtml.cs
@@ -5,22 +5,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationSecurity.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace ApplicationSecurity.Pages
 {
-    [Authorize(Roles = "Admin")]
     public class PrivacyModel : PageModel
     {
         private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        private readonly UserManager<ApplicationUser> _userManager;
+        
+        public PrivacyModel(ILogger<PrivacyModel> logger, UserManager<ApplicationUser> userManager)
         {
             _logger = logger;
+            _userManager = userManager;
         }
 
-        public void OnGet()
+        private async Task<bool> IsUserAdminAsync()
         {
+            var user = await _userManager.GetUserAsync(User);
+            return user?.Id == Guid.Parse("F3148AF2-F58C-4895-A304-08D9EA58BD13");
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            if (!await IsUserAdminAsync())
+            {
+                return Forbid();
+            }
+
+            return Page();
+        }
+        
+        public async Task<IActionResult> OnPost()
+        {
+            if (!await IsUserAdminAsync())
+            {
+                return Forbid();
+            }
+
+            return Page();
         }
     }
 }
