12a8b87018d83630541b54bfc1baf98a04cfd1f3
FrancisRalph <francis341593@gmail.com>
2022-02-10 00:03:49 +0800

    fix log out bug after enabling 2fa for first time

--- Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -19,17 +19,20 @@ namespace ApplicationSecurity.Areas.Identity.Pages.Account.Manage
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<EnableAuthenticatorModel> _logger;
         private readonly UrlEncoder _urlEncoder;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
         private const string AuthenticatorUriFormat = "otpauth://totp/{0}:{1}?secret={2}&issuer={0}&digits=6";
 
         public EnableAuthenticatorModel(
             UserManager<ApplicationUser> userManager,
             ILogger<EnableAuthenticatorModel> logger,
-            UrlEncoder urlEncoder)
+            UrlEncoder urlEncoder,
+            SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
             _logger = logger;
             _urlEncoder = urlEncoder;
+            _signInManager = signInManager;
         }
 
         public string SharedKey { get; set; }
@@ -120,6 +123,8 @@ namespace ApplicationSecurity.Areas.Identity.Pages.Account.Manage
             {
                 await _userManager.ResetAuthenticatorKeyAsync(user);
                 unformattedKey = await _userManager.GetAuthenticatorKeyAsync(user);
+                
+                await _signInManager.RefreshSignInAsync(user);
             }
 
             SharedKey = FormatKey(unformattedKey);
